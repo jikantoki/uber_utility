ddc4535d03e279388dff58821ba49299cc69b9d2
jikantoki <61790160+jikantoki@users.noreply.github.com>
2024-09-30 23:08:12 +0900

    タイトル修正

--- js/setup.js
+++ js/setup.js
@@ -9,7 +9,7 @@ export default {
    * @returns 引数に合わせて設定したら0、デフォルトのまま設定したら1
    */
   setTitle: (newTitle) => {
-    let siteName = PackageJson.name
+    let siteName = 'UberUTL'
     siteName = Functions.ifEnglishStartUpper(siteName)
     let pageTitle
     let returnCode

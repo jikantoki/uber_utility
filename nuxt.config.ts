4fc5d6326519e1ca75e448b90791f091d0d555f3
jikantoki <61790160+jikantoki@users.noreply.github.com>
2024-10-25 16:01:53 +0900

    unset new api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -54,13 +54,6 @@ export default defineNuxtConfig({
     define: {
       'process.env.DEBUG': false,
     },
-    css: {
-      preprocessorOptions: {
-        scss: {
-          api: 'modern-compiler',
-        },
-      },
-    },
   },
   css: [
     '@/assets/main.scss',

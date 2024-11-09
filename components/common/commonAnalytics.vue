654411e73ea32aa9be4336c43162161120e80bb6
jikantoki <61790160+jikantoki@users.noreply.github.com>
2024-11-09 18:49:45 +0900

    analytics画面実装（数字正しくない）

---
+++ components/common/commonAnalytics.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+.center
+  .commission
+    .commission-item
+        .text-h6 収支
+        div(style="display: flex; align-items: flex-end; margin: 8px 0;")
+          .text-h2.ma-0 {{ data.commission }}
+          .text-h6(style="margin-left: 0.5em;") 円
+    .hourly-and-operate-time(style="display:flex;")
+      .hourly.commission-item.mr-2(style="width:50%;")
+        .text-h6 時給
+        .hourly-text.text-h5 {{ calcHourly(data.commission, data.time) }}円
+      .operate-time.commission-item.ml-2(style="width:50%;")
+        .text-h6 稼働時間
+        .operate-time-text.text-h5 {{ Math.floor(data.time / 60) }}時間{{ Math.floor(data.time % 60) }}分
+</template>
+
+<script>
+import mixins from '~/mixins/mixins'
+export default {
+  mixins: [mixins],
+  props: {
+    /** 稼働データ（1件のみ受け付け） */
+    data: {
+      type: Object,
+      default: {
+        commission: 0,
+        dateUnixtime: 1730732400,
+        memo: '空のデータ',
+        time: 0,
+        workId: null,
+      },
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.hitsuyou-mudana-space {
+  height: 5em;
+  width: 100%;
+}
+</style>
--- pages/analytics.vue
+++ pages/analytics.vue
@@ -6,7 +6,7 @@
       .contents
         .other-content(v-for="button of shushiButtons")
           .a-cover(v-ripple)
-            a.content-a(:href="button.href")
+            a.content-a(@click="activeDialog=true")
               .button-inner.pl-4.py-4
                 v-icon {{ button.icon }}
                 p.text-h7.ml-2 {{ button.name }}
@@ -16,21 +16,40 @@
       .contents
         .other-content(v-for="button of riekiButtons")
           .a-cover(v-ripple)
-            a.content-a(:href="button.href")
+            a.content-a(@click="activeDialog=true")
               .button-inner.pl-4.py-4
                 v-icon {{ button.icon }}
                 p.text-h7.ml-2 {{ button.name }}
                 p.text-h7.right.pr-4 >
           hr.ma-0
+  v-dialog(v-model="activeDialog" max-width="640px")
+    v-card
+      .analytics-item.my-4.mx-6
+        commonAnalytics(:data="commission[0]")
+        .details.mt-4
+          .detail(v-for="detail in detailList")
+            p {{ detail.title }}:
+            p.right {{ detail.value }}{{ detail.unit }}
+      v-card-actions
+        v-btn(
+          @click="activeDialog=false"
+          variant="elevated"
+          color="var(--accent-color)"
+          style="color: white;"
+        ) 閉じる
 </template>
 
 <script>
 import metaFunctions from '~/js/metaFunctions'
 import mixins from '~/mixins/mixins'
 import Setup from '~/js/setup'
+import commonAnalytics from '~/components/common/commonAnalytics.vue'
 
 export default {
   mixins: [mixins],
+  components: {
+    commonAnalytics,
+  },
   setup() {
     //サーバーサイドで仮のタイトルを設定、mountedで言語ごとに再設定する
     Setup.setTitle('Analytics')
@@ -82,10 +101,56 @@ export default {
           href: '/settings',
         },
       ],
+      detailList: {
+        /** 稼働日数 */
+        daycount: {
+          title: '稼働日数',
+          value: 0,
+          unit: '日',
+        },
+        /** 日割の平均稼働時間 */
+        averageTime: {
+          title: '平均稼働時間',
+          value: '0時間0分',
+          unit: '',
+        },
+        /** 日割の平均収支 */
+        averageCommission: {
+          title: '平均収支',
+          value: '0',
+          unit: '円',
+        },
+        /** 日割の最大収支 */
+        maxCommission: {
+          title: '最大収支',
+          value: '0',
+          unit: '円',
+        },
+      },
+      /** 稼働実績リスト */
+      commission: [],
+      /** ダイアログ開くフラグ */
+      activeDialog: false,
     }
   },
-  mounted() {
+  async mounted() {
     this.setTitle('収支分析')
+
+    //commissionリスト取得
+    if (this.userStore && this.userStore.userId) {
+      const getWork = await this.sendAjaxWithAuth('/getWork.php', {
+        id: this.userStore.userId,
+        token: this.userStore.userToken,
+      })
+      const worklist = JSON.parse(getWork.body.work)
+
+      worklist.sort((a, b) => {
+        return b.dateUnixtime - a.dateUnixtime
+      })
+
+      this.commission = worklist
+      console.log(worklist)
+    }
   },
 }
 </script>
@@ -94,6 +159,7 @@ export default {
 .content-a {
   text-decoration: none;
   color: unset;
+  cursor: pointer;
   .button-inner {
     display: flex;
     position: relative;
@@ -106,5 +172,13 @@ export default {
 }
 hr {
   width: 100% !important;
+}
+.details {
+  .detail {
+    display: flex;
+    .right {
+      margin-left: auto;
+    }
+  }
 }
 </style>

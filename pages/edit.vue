a9381af2e9a4cf1bec766a61b1a2b7b8706fda44
jikantoki <61790160+jikantoki@users.noreply.github.com>
2024-11-13 15:25:06 +0900

    修正

--- pages/edit.vue
+++ pages/edit.vue
@@ -134,13 +134,6 @@
           color="var(--accent-color)"
           style="color: white;"
         ) 保存
-  .fixed-button
-    v-btn(
-      size="large"
-      color="var(--accent-color)"
-      style="height: auto; width: 100%; border-radius: var(--border-radius); color: white;"
-      @click="clearWorkData();editDialog=true;editMode=false"
-    ) 新規追加
   component-button(
     v-if="userStore && userStore.userId"
     icon="mdi-plus"

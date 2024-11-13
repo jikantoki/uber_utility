15342f063199642eed33b80710b97a2ca1f72d97
jikantoki <61790160+jikantoki@users.noreply.github.com>
2024-11-13 16:27:17 +0900

    カレンダーと燃費の画面

---
+++ pages/calendar.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+.about
+  h1 カレンダー！
+  p 開発中
+</template>
+
+<script>
+import metaFunctions from '~/js/metaFunctions'
+import mixins from '~/mixins/mixins'
+import Setup from '~/js/setup'
+
+export default {
+  mixins: [mixins],
+  setup() {
+    //サーバーサイドで仮のタイトルを設定、mountedで言語ごとに再設定する
+    Setup.setTitle('Calendar')
+    Setup.setDescription('稼働履歴をカレンダーで見る')
+  },
+  mounted() {
+    this.setTitle('カレンダー')
+  },
+}
+</script>
---
+++ pages/fuel.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+.about
+  h1 燃費計算！
+  p 開発中
+</template>
+
+<script>
+import metaFunctions from '~/js/metaFunctions'
+import mixins from '~/mixins/mixins'
+import Setup from '~/js/setup'
+
+export default {
+  mixins: [mixins],
+  setup() {
+    //サーバーサイドで仮のタイトルを設定、mountedで言語ごとに再設定する
+    Setup.setTitle('Fuel')
+    Setup.setDescription('燃費計算')
+  },
+  mounted() {
+    this.setTitle('燃費')
+  },
+}
+</script>

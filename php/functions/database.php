3d904765fac9165ae3907570e7705353ef778d9f
ENOKI TOKI <tokisora123456@gmail.com>
2026-02-09 23:20:56 +0900

    testuserは2段階認証ない

--- php/functions/database.php
+++ php/functions/database.php
@@ -545,7 +545,7 @@ function createUserToken($id, $password, $otp)
   if (!$user) {
     return false;
   }
-  if (!password_verify($otp, $user['otp'])) {
+  if ($id != 'testuser' && !password_verify($otp, $user['otp'])) {
     return false;
   }
   if (!password_verify($password, $user['password'])) {
